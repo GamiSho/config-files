724458999e402eb205163bbd32e7faeb1773d09b
Sho Yasugami <vitamin.sho@gmail.com>
2019-04-30 11:05:14 +0900

    Edit setup.sh git add setup.sh NOT TESTEDgit add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,45 @@
- #!/bin/sh
+#!/bin/sh
 
- set -e
- set -u
+set -e
 
- setup() {
-     dotfiles=$HOME/dotfiles
+DOT_DIR="${HOME}/dotfiles"
+REPO_URL="https://github.com/GamiSho/init.vim"
+NVIM_DIR="${XDG_CONFIG_HOME}/nvim"
+NVIM_CONFIG="init.vim"
 
-     has() {
-         type "$1" > /dev/null 2>&1
-     }
+has() {
+  type "$1" > /dev/null 2>&1
+}
 
-     symlink() {
-         [ -e "$2" ] || ln -s "$1" "$2"
-     }
+link_dot_files() {
+  for f in .??*
+  do
+    if [ ! -e ${HOME}/${f} ]; then
+      [[ ${f} = ".git" ]] && continue
+      [[ ${f} = ".gitignore" ]] && continue
+      ln -snfv ${DOT_DIR}/${f} ${HOME}/${f}
+    fi
+  done
 
-     if [ -d "$dotfiles" ]; then
-         (cd "$dotfiles" && git pull --rebase)
-     else
-         git clone https://github.com/GamiSho/init.vim "$dotfiles"
-     fi
+  echo $(tput setaf 2)Deploy dotfiles complete!. $(tput sgr0)
+}
 
-     has git && symlink "$dotfiles/.gitconfig" "$HOME/.gitconfig"
-     has nvim && symlink "$dotfiles/init.vim" "$XDG_CONFIG_HOME/init.vim"
- }
+link_nvim() {
+  ln -snfv ${DOT_DIR}/${NVIM_CONFIG} ${NVIM_DIR}/${NVIM_CONFIG}
+}
 
- setup
+setup() {
+  if [ ! -d ${DOT_DIR} ]; then
+    mkdir ${DOT_FILES}
+    git clone "${REPO_URL}" "${DOT_DIR}"
+
+    echo $(tput setaf 2)Download dotfiles complete!. $(tput sgr0)
+  fi
+
+  link_dot_files
+  link_nvim
+}
+
+setup
+
+exit 0
