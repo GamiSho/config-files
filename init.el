84cb452b1800992a7d5d25d001b71f4ba54c640e
Sho Yasugami <vitamin.sho@gmail.com>
2022-11-29 16:33:17 +0900

    Add emacs

---
+++ init.el
@@ -0,0 +1,34 @@
+(require 'package)
+
+;; HTTP
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/") t)
+
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(modus-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;
+;; theme
+;;
+(require 'modus-themes)
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs nil
+      modus-themes-region '(bg-only no-extend))
+(modus-themes-load-themes)
+(modus-themes-load-vivendi)
+(global-set-key (kbd "<f5>") 'modus-themes-toggle)
+
+(electric-pair-mode 1)
