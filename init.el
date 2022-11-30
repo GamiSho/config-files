09c15772c39a51220a80fb483c3f724685f951ca
Sho Yasugami <vitamin.sho@gmail.com>
2022-11-30 18:45:12 +0900

    Update emacs settings

--- init.el
+++ init.el
@@ -1,40 +1,68 @@
-(require 'package)
+(setq mac-option-modifier 'meta)
 
-;; HTTP
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-;;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/") t)
+(defun shasugami/add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (unless (file-exists-p default-directory)
+          (make-directory default-directory))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
 
-(package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(modus-themes)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(shasugami/add-to-load-path "elisp" "conf")
 
-;;
-;; theme
-;;
-(require 'modus-themes)
-(setq modus-themes-italic-constructs t
-      modus-themes-bold-constructs nil
-      modus-themes-region '(bg-only no-extend))
-(modus-themes-load-themes)
-(modus-themes-load-vivendi)
-(global-set-key (kbd "<f5>") 'modus-themes-toggle)
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
+(load custom-file)
 
+(prog1 "prepare leaf"
+  (prog1 "package"
+    (custom-set-variables
+     '(package-archives '(("org"   . "https://orgmode.org/elpa/")
+                          ("melpa" . "https://melpa.org/packages/")
+                          ("gnu"   . "https://elpa.gnu.org/packages/"))))
+    (package-initialize))
+
+  (prog1 "leaf"
+    (unless (package-installed-p 'leaf)
+      (unless (assoc 'leaf package-archive-contents)
+        (package-refresh-contents))
+      (condition-case err
+          (package-install 'leaf)
+        (error
+         (package-refresh-contents)       ; renew local melpa cache if fail
+         (package-install 'leaf))))
+
+    (leaf leaf-keywords
+      :ensure t
+      :config (leaf-keywords-init)))
+
+  (prog1 "optional packages for leaf-keywords"
+    ;; optional packages if you want to use :hydra, :el-get,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t
+      :custom ((el-get-git-shallow-clone  . t)))))
+
+(define-key global-map (kbd "C-t") 'other-window)
+(define-key global-map (kbd "C-c k") 'kill-buffer-and-window)
+
+(leaf smart-mode-line
+  :ensure t
+  :custom ((sml/no-confirm-load-theme . t)
+           (sml/theme . 'dark)
+           (sml/shorten-directory . -1))
+  :config
+  (sml/setup))
+
+(column-number-mode t)
+(global-linum-mode t)
+(setq inhibit-startup-message t)
+(setq-default indent-tabs-mode nil)
 (electric-pair-mode 1)
-
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (put 'upcase-region 'disabled nil)
-
 (add-to-list 'default-frame-alist '(cursor-type . bar))
