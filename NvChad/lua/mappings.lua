8b7b53a64679d6d8346e5baf82c14d15f53aee4a
Sho Yasugami <vitamin.sho@gmail.com>
2024-05-01 16:45:05 +0900

    Add NvChad directory

---
+++ NvChad/lua/mappings.lua
@@ -0,0 +1,24 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+local options = { noremap = true }
+
+map('n', 'j', 'gj', options)
+map('n', 'k', 'gk', options)
+map('n', 'L', '$', options)
+map('n', 'H', '^', options)
+map('i', 'jk', '<esc>', { noremap = true, silent = true })
+map('i', '"', '""<ESC>i', options)
+map('i', '\'', '\'\'<ESC>i', options)
+map('i', '(', '()<ESC>i', options)
+map('i', '[', '[]<ESC>i', options)
+map('i', '{', '{}<ESC>i', options)
+map('i', '{<CR>', '{<CR>}<ESC>O', options)
+map('i', '<C-f>', '<right>', options)
+map('i', '<C-b>', '<left>', options)
+map('i', '<C-p>', '<Up>', options)
+map('i', '<C-n>', '<Down>', options)
+map('t', '<ESC>', '<C-\\><C-n>', options)
+
