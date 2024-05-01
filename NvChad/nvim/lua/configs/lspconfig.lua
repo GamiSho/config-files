6ac37c7d48a271173acdb1f6693af96799bb4eff
Sho Yasugami <vitamin.sho@gmail.com>
2024-05-01 19:19:24 +0900

    ElixirのLSP設定
    ディレクトリを変更

--- NvChad/nvim/lua/configs/lspconfig.lua
+++ NvChad/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "elixirls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -16,7 +16,8 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.elixirls.setup {
+  cmd = { "/Users/genronnewimac/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" };
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
---
+++ NvChad/nvim/lua/plugins/init.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc", "html", "css", "elixir",
+        "javascript", "typescript", "tsx"
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua", "elixir-ls"
+  		},
+  	},
+  },
+}
