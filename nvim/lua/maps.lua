ea2c0c0a78c1f60a4fec94fac6f6287fbe2d897a
Sho Yasugami <vitamin.sho@gmail.com>
2024-05-01 14:57:10 +0900

    Fix: bug

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -4,7 +4,7 @@ map('n', '<space>', '', {})
 vim.g.mapleader = ' '
 
 
-options = { noremap = true }
+local options = { noremap = true }
 map('n', '<leader><esc>', ':nohlsearch<cr>', options)
 map('n', '<leader>n', ':bnext<cr>', options)
 map('n', '<leader>p', ':bprev<cr>', options)
@@ -14,10 +14,10 @@ map('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>',
 map('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', options)
 map('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>', options)
 
-map('n', 'j', 'gj', optioins)
-map('n', 'k', 'gk', optioins)
-map('n', 'L', '$', optioins)
-map('n', 'H', '^', optioins)
+map('n', 'j', 'gj', options)
+map('n', 'k', 'gk', options)
+map('n', 'L', '$', options)
+map('n', 'H', '^', options)
 map('i', 'jk', '<esc>', { noremap = true, silent = true })
 map('i', '"', '""<ESC>i', options)
 map('i', '\'', '\'\'<ESC>i', options)
